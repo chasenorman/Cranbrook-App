e36f78de9ccd5e28a2c6d08c024e0feb991015a4
Aziz <azizulin2@gmail.com>
2018-02-17 22:41:13 -0500

    renames files, changes how the sign-out button works. Has all the assets and constraints. Doesn't include the small visual tweaks. The sign-out bug is still here.

---
+++ Cranbrook/Controller/AccountController.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewController.swift
+//  Cranbrook
+//
+//  Created by Aziz Zaynutdinov on 2/16/18.
+//  Edited by Chase Norman.
+//  Copyright © 2018 Chase Norman. All rights reserved.
+//
+import UIKit
+
+class AccountController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    @IBAction func signOut(_ sender: UIButton) {
+        UserDefaults.standard.removeObject(forKey: "username");
+        UserDefaults.standard.removeObject(forKey: "password")
+        UserDefaults.standard.removeObject(forKey: "token")
+        performSegue(withIdentifier: "signOut", sender: nil)
+    }
+}
